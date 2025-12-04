7216ae14a61cd3850e94c7150a8f963ccb00224f
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-12-04 00:15:19 -0400

    Actualizando las vistas que se muestran al abrir el programa

--- AgenciadeTours/AgenciadeTours/Program.cs
+++ AgenciadeTours/AgenciadeTours/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Paises}/{action=Crear}/{id?}");
 
 app.Run();
